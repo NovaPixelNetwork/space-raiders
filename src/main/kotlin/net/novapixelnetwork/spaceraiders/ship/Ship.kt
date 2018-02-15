2f2f47e1a25b72d7b9fa61a1d0258994e226f167
Nicholas Harris (ScarabCoder) <scarabcoder@gmail.com>
2018-02-15 11:09:40 -0800

    Setting up Travis CI

--- src/main/kotlin/net/novapixelnetwork/spaceraiders/ship/Ship.kt
+++ src/main/kotlin/net/novapixelnetwork/spaceraiders/ship/Ship.kt
@@ -1,12 +1,20 @@
 package net.novapixelnetwork.spaceraiders.ship
 
 import java.util.*
+import kotlin.collections.HashMap
 
 /**
  * Created by owner on 1/5/2018.
  */
 
 class Ship(val id: Int, val hangar: Hangar, val size: Hangar.Size, val owner: UUID, var name: String) {
+
+    init {
+
+    }
+
+    val engineData: HashMap<Engine, EngineData> = HashMap()
+    val hullData: HashMap<Hull, HullData> = HashMap()
 
     var status = ShipStatus.HANGAR
 
@@ -17,8 +25,14 @@ class Ship(val id: Int, val hangar: Hangar, val size: Hangar.Size, val owner: UU
                     "owner VARCHAR(36) NOT NULL," +
                     "name VARCHAR(16) NULL," +
                     "hangar INT NOT NULL," +
+                    "engine VARCHAR(32) NOT NULL, " +
+                    "hull VARCHAR(32) NOT NULL, " +
                     "PRIMARY KEY (`id`));"
         }
     }
+
+    data class EngineData(val engine: Engine, var unlocked: Boolean)
+
+    data class HullData(val hull: Hull, var unlocked: Boolean, var turretsUnlocked: Int)
 
 }
